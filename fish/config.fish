5202ba5d359a380c9e733852726d962ad232c6ae
jirathip-k <jkun0008@student.monash.edu>
2023-10-23 19:34:50 +1100

    unify from linux side

--- fish/config.fish
+++ fish/config.fish
@@ -7,15 +7,16 @@ set -gx EDITOR "nvim"
 alias vim=nvim
 alias python=python3
 
+if status is-interactive
+    and not set -q TMUX
+    exec tmux
+end
+
 # Specific settings for macOS
 if test (uname) = "Darwin"
     set -gx PATH /opt/homebrew/bin $PATH
 
-    if status is-interactive
-        and not set -q TMUX
-        exec tmux
-    end
-end
+ end
 
 # Specific settings for Linux
 if test (uname) = "Linux"
