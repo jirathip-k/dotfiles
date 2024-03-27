6d0d25441634e5f2179af49a7eec26f83022f461
jirathip-k <115384744+jirathip-k@users.noreply.github.com>
2024-03-27 20:18:01 +0700

    resolve conflict

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -68,47 +68,5 @@ return {
                 },
             },
         })
-
-        local ts_utils = require("nvim-treesitter.ts_utils")
-
-        local function tabout()
-            local bufnr = vim.api.nvim_get_current_buf()
-            local cursor = vim.api.nvim_win_get_cursor(0)
-            local cursor_row, cursor_col = cursor[1] - 1, cursor[2] + 1
-            local node = ts_utils.get_node_at_cursor()
-            local exit_chars = "[,%:%;%)%}%]%>]"
-            local default_tab = vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
-            local lines_count = vim.api.nvim_buf_line_count(0)
-
-            while node do
-                local node_type = node:type()
-
-                if node_type == "string" or node_type == "string_fragment" or node_type == "string_literal" then
-                    local _, _, end_row, end_col = node:range()
-
-                    if end_row < lines_count then
-                        vim.api.nvim_win_set_cursor(0, { end_row + 1, end_col })
-                        return
-                    end
-                end
-                node = node:parent()
-            end
-
-            if cursor_row + 1 <= lines_count then
-                local line = vim.api.nvim_buf_get_lines(bufnr, cursor_row, cursor_row + 1, false)[1]
-                if line and cursor_col <= #line then
-                    if line:sub(cursor_col, cursor_col):match(exit_chars) then
-                        vim.api.nvim_win_set_cursor(0, { cursor_row + 1, cursor_col + 1 })
-                    else
-                        vim.api.nvim_feedkeys(default_tab, "n", true)
-                    end
-                elseif cursor_col > #line and cursor_row + 1 < lines_count then
-                    local next_indent = vim.fn.indent(cursor_row + 2)
-                    vim.api.nvim_win_set_cursor(0, { cursor_row + 2, next_indent })
-                end
-            end
-        end
-
-        vim.keymap.set("i", "<Tab>", tabout, { desc = "Tabout", silent = true, noremap = true })
     end,
 }
