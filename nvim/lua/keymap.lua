800104039d94c2fb75b6106e38c1f307e09b9abe
jirathip-k <115384744+jirathip-k@users.noreply.github.com>
2024-06-09 18:47:38 +0700

    dadbod nvim

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -31,6 +31,7 @@ local telescope_builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>f", telescope_builtin.find_files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>g", telescope_builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>c", telescope_builtin.commands, { desc = "Commands" })
 vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = "Undo Tree" })
 
 -- Open parent dir
--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,48 +1,50 @@
 return {
+	"mfussenegger/nvim-dap-python",
+	ft = "python",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"theHamsta/nvim-dap-virtual-text",
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		local dap = require("dap")
+		local ui = require("dapui")
+		local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
 
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "nvim-neotest/nvim-nio",
-        "theHamsta/nvim-dap-virtual-text",
-        "mfussenegger/nvim-dap"
-    },
-    config = function()
-        local dap = require("dap")
-        local ui = require("dapui")
-        local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+		require("dapui").setup()
+		require("nvim-dap-virtual-text").setup()
+		require("dap-python").setup(path)
 
-        require("dapui").setup()
-        require("nvim-dap-virtual-text").setup()
-        require("dap-python").setup(path)
+		vim.keymap.set(
+			"n",
+			"<leader>db",
+			dap.toggle_breakpoint,
+			{ desc = "Toggle breakpoint", noremap = true, silent = true }
+		)
+		vim.keymap.set("n", "<leader>dr", dap.run_to_cursor, { desc = "Run to cursor", noremap = true, silent = true })
 
-        vim.keymap.set("n", "<leader>bb", dap.toggle_breakpoint,
-            { desc = "Toggle breakpoint", noremap = true, silent = true })
-        vim.keymap.set("n", "<leader>br", dap.run_to_cursor, { desc = "Run to cursor", noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>d?", function()
+			require("dapui").eval(nil, { enter = true })
+		end, { desc = "Eval var under cursor", noremap = true, silent = true })
 
-        vim.keymap.set("n", "<leader>b?", function()
-            require("dapui").eval(nil, { enter = true })
-        end, { desc = "Eval var under cursor", noremap = true, silent = true })
-
-        vim.keymap.set("n", "<leader>bc", dap.continue, { desc = "Continue", noremap = true, silent = true })
-        -- vim.keymap.set("n", "<leader>b2", dap.step_into, { desc = "Step into", noremap = true, silent = true })
-        -- vim.keymap.set("n", "<leader>b3", dap.step_over, { desc = "Step over", noremap = true, silent = true })
-        -- vim.keymap.set("n", "<leader>b4", dap.step_out, { desc = "Step out", noremap = true, silent = true })
-        -- vim.keymap.set("n", "<leader>b5", dap.step_back, { desc = "Step back", noremap = true, silent = true })
-        vim.keymap.set("n", "<leader>bs", dap.restart, { desc = "Restart", noremap = true, silent = true })
-        dap.listeners.before.attach.dapui_config = function()
-            ui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            ui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            ui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            ui.close()
-        end
-    end
-
+		vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue", noremap = true, silent = true })
+		-- vim.keymap.set("n", "<leader>b2", dap.step_into, { desc = "Step into", noremap = true, silent = true })
+		-- vim.keymap.set("n", "<leader>b3", dap.step_over, { desc = "Step over", noremap = true, silent = true })
+		-- vim.keymap.set("n", "<leader>b4", dap.step_out, { desc = "Step out", noremap = true, silent = true })
+		-- vim.keymap.set("n", "<leader>b5", dap.step_back, { desc = "Step back", noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>ds", dap.restart, { desc = "Restart", noremap = true, silent = true })
+		dap.listeners.before.attach.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			ui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			ui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			ui.close()
+		end
+	end,
 }
