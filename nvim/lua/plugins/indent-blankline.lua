0a9e8365b4a014c0c13b18e46fd4f78500b428c8
jirathip-k <115384744+jirathip-k@users.noreply.github.com>
2024-06-09 19:12:05 +0700

    set up default harpoon for deletion

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,1 +1,7 @@
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	config = function()
+		require("ibl").setup()
+	end,
+}
